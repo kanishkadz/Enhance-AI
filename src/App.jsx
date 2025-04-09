6dd51256892cb38558f953aebbf50d69881624b6
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-09 22:00:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8 px-4">
         <div className="text-center mb-8">
-          <h1 className="text-5xl font-bold text-gray-800">EnhanceAI : AI Image Enhancer</h1>
+          <h1 className="text-5xl font-bold text-gray-800 mb-2">EnhanceAI : AI Image Enhancer</h1>
           <p className="text-lg text-gray-500">Upload your Image and let AI enhance it in seconds !</p>
         </div>
 
