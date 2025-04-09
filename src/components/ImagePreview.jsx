9e2a5d39e2c497204a52fc4c39f0be686db839d9
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-09 22:18:41 +0530

    Update ImagePreview.jsx

--- src/components/ImagePreview.jsx
+++ src/components/ImagePreview.jsx
@@ -4,7 +4,8 @@ const ImagePreview = () => {
   return (
     <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-4xl">
       <div className="bg-white shadow-lg rounded-xl overflow-hidden">
-        <h2 className="text-xl font-semibold text-center bg-gray-800 text-white">Original Image</h2>
+        <h2 className="text-xl font-semibold text-center bg-gray-800 text-white py-2">Original Image</h2>
+        <img src="" alt="" className="w-full h-full object-cover" />
       </div>
 
       <div></div>
