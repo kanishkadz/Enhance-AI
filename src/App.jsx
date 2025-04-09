7cd29fa6241fdd3a99eb8d5052e319961a7bae16
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-09 21:57:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8 px-4">
         <div className="text-center mb-8">
-          <h1></h1>
+          <h1>EnhanceAI : AI Image Enhancer</h1>
           <p></p>
         </div>
 
