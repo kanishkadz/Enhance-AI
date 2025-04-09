8db8c700fb4a4174e582b5f05b6d4b261cc68438
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-09 22:20:16 +0530

    Update ImagePreview.jsx

--- src/components/ImagePreview.jsx
+++ src/components/ImagePreview.jsx
@@ -3,12 +3,17 @@ import React from 'react'
 const ImagePreview = () => {
   return (
     <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-4xl">
+      {/* Original Image */}
       <div className="bg-white shadow-lg rounded-xl overflow-hidden">
         <h2 className="text-xl font-semibold text-center bg-gray-800 text-white py-2">Original Image</h2>
         <img src="" alt="" className="w-full h-full object-cover" />
       </div>
 
-      <div></div>
+      {/* Enhanced Image */}
+      <div className="bg-white shadow-lg rounded-xl overflow-hidden">
+        <h2 className="text-xl font-semibold text-center bg-gray-800 text-white py-2">Original Image</h2>
+        <img src="" alt="" className="w-full h-full object-cover" />
+      </div>
     </div>
   )
 }
