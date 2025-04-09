a23cb78af16d0a51b347e9cbf548c8f179b776b8
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-09 21:54:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./components/Home";
 function App() {
   return (
     <>
-      <div>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8 px-4">
         Header
         <Home />
         Footer
