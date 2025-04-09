ad31cbddc34d394813d6a9e6d537aef5c47f30e0
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-09 22:07:28 +0530

    Update ImageUpload.jsx

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const ImageUpload = () => {
   return (
     <div className='bg-white shadow-lg rounded-2xl p-6 w-full max-w-2xl'>
-      <label htmlFor="fileInput" className="block w-full cursor-pointer border-2 border-dashed border-gray-300 rounded-b-lg"></label>
+      <label htmlFor="fileInput" className="block w-full cursor-pointer border-2 border-dashed border-gray-300 rounded-b-lg">
+        <p>Click & drag to upload your image</p>
+      </label>
     </div>
   )
 }
