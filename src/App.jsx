303025a9bbc5fa6bb8813575a3cfac8e8185417c
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-09 21:56:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,16 @@ function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8 px-4">
-        Header
+        <div className="text-center mb-8">
+          <h1></h1>
+          <p></p>
+        </div>
+
         <Home />
-        Footer
+
+        <div className="text-lg text-gray-500">
+          Upload your Image and let AI enhance it in seconds !          
+        </div>
       </div>
     </>
   )
