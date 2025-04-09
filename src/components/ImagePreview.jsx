8392cb98a51931868a487711a900cda88d8a9800
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-09 22:16:51 +0530

    Update ImagePreview.jsx

--- src/components/ImagePreview.jsx
+++ src/components/ImagePreview.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const ImagePreview = () => {
   return (
     <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-4xl">
-      <div></div>
+      <div className="bg-white shadow-lg rounded-xl overflow-hidden">
+        <h2>Original Image</h2>
+      </div>
 
       <div></div>
     </div>
