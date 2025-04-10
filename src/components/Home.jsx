d9b5165f72e4f55725da07a433b3e3a829d1f58d
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-10 21:45:53 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,11 @@ import ImageUpload from "./ImageUpload"
 import ImagePreview from "./ImagePreview"
 
 const Home = () => {
+  const [uploadImage, setUploadImage] = useState(null);
+  const [enhancedImage, setEnhancedImage] = useState(null);
+
+
+
   return (
     <>
       <ImageUpload />
