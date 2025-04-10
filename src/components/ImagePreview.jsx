6ee481dacf43e7979be96cdca7d40ce9702e66dd
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-10 22:04:13 +0530

    Update ImagePreview.jsx

--- src/components/ImagePreview.jsx
+++ src/components/ImagePreview.jsx
@@ -5,16 +5,18 @@ const ImagePreview = (props) => {
     <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-4xl">
       {/* Original Image */}
       <div className="bg-white shadow-lg rounded-xl overflow-hidden">
+
         <h2 className="text-xl font-semibold text-center bg-gray-800 text-white py-2">Original Image</h2>
-        {props.uploaded ? <img src={props.uploaded} alt="" className="w-full h-full object-cover" /> :
-          <div className="flex items-center justify-center h-80 bg-gray-200">No Image Selected</div>}
+        {props.uploaded ? <img src={props.uploaded} alt="" className="w-full h-full object-cover" /> : <div className="flex items-center justify-center h-80 bg-gray-200">No Image Selected</div>}
       </div>
 
       {/* Enhanced Image */}
       <div className="bg-white shadow-lg rounded-xl overflow-hidden">
         <h2 className="text-xl font-semibold text-center bg-blue-800 text-white py-2">Enhanced Image</h2>
-        <img src="" alt="" className="w-full h-full object-cover" />
-        <div className="flex items-center justify-center h-80 bg-gray-200">No Enhanced Image</div>
+
+        {props.enhanced ? <img src="" alt="" className="w-full h-full object-cover" /> : <div className="flex items-center justify-center h-80 bg-gray-200">No Enhanced Image</div>}
+        
+        
       </div>
     </div>
   )
