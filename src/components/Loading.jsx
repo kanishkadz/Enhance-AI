3bdce21c831c9e228a224414b89b0038f509f90a
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-10 22:27:29 +0530

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loading = () => {
   return (
     <div className="flex justify-center items-center h-full">
-      <div className="animate-spin border-4 border-t-transparent w-[50px] h-[50px] rounded-full border-red-300"></div>
+      <div className="animate-spin border-4 border-t-transparent w-10 h-10 scale-150 rounded-full border-red-300"></div>
     </div>
   )
 }
