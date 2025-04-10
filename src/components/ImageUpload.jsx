71787074e900843fdb16fa4efd9e112ab8d16670
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-10 21:49:31 +0530

    Update ImageUpload.jsx

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 const ImageUpload = () => {
+  const ShowImageHandler = (e) => {
+    e.target.files;
+  }
+
+
   return (
     <div className='bg-white shadow-lg rounded-2xl p-6 w-full max-w-2xl'>
       <label htmlFor="fileInput" className="block w-full cursor-pointer border-2 border-dashed border-gray-300 rounded-b-lg p-6 text-center hover:border-blue-500 transition-all">
-        <input type="file" id="fileInput" className="hidden" />
+        <input type="file" id="fileInput" className="hidden" onChange={ShowImageHandler} />
         <span className="text-lg font-medium text-gray-600">Click & drag to upload your image</span>
       </label>
     </div>
