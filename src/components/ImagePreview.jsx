c1fa527cb09ce7faae93df64bdd0bdb81a8b48ef
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-10 22:09:29 +0530

    Update ImagePreview.jsx

--- src/components/ImagePreview.jsx
+++ src/components/ImagePreview.jsx
@@ -14,7 +14,7 @@ const ImagePreview = (props) => {
       <div className="bg-white shadow-lg rounded-xl overflow-hidden">
         <h2 className="text-xl font-semibold text-center bg-blue-800 text-white py-2">Enhanced Image</h2>
 
-        {props.enhanced ? <img src="" alt="" className="w-full h-full object-cover" /> : <div className="flex items-center justify-center h-80 bg-gray-200">No Enhanced Image</div>}
+        {props.enhanced && !props.loading (<img src="" alt="" className="w-full h-full object-cover" />) : <div className="flex items-center justify-center h-80 bg-gray-200">No Enhanced Image</div>}
         
         
       </div>
