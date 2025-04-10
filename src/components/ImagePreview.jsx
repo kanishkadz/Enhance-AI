0083d5155878a30bebf265540c75ed569bdbb290
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-10 21:58:31 +0530

    Update ImagePreview.jsx

--- src/components/ImagePreview.jsx
+++ src/components/ImagePreview.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const ImagePreview = () => {
+const ImagePreview = (props) => {
   return (
     <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-4xl">
       {/* Original Image */}
       <div className="bg-white shadow-lg rounded-xl overflow-hidden">
         <h2 className="text-xl font-semibold text-center bg-gray-800 text-white py-2">Original Image</h2>
-        <img src="" alt="" className="w-full h-full object-cover" />
+        <img src={props.uploaded} alt="" className="w-full h-full object-cover" />
         <div className="flex items-center justify-center h-80 bg-gray-200">No Image Selected</div>
       </div>
 
