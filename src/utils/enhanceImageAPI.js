730dcd1c32daf9f8100ac8046a42c074194574e3
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-11 19:17:25 +0530

    Update enhanceImageAPI.js

--- src/utils/enhanceImageAPI.js
+++ src/utils/enhanceImageAPI.js
@@ -1,3 +1,10 @@
 export const enhancedImageAPI = async(file) => {
-    return "hello";
+    try {
+        //code to upload image
+
+        //fetch enhanced image        
+    } catch (error) {
+        console.log("Error enhancing image:", error.message);
+        
+    }
 };
