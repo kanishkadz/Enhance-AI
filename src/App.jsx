1f83fe111a5247b2435d0ad553a5e2592c5e3050
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-11 19:52:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ const App = () => {
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8 px-4 ">
             <div className="text-center mb-8">
                 <h1 className="text-5xl font-bold text-gray-800 mb-2">
-                    AI Image Enhancer{" "}
+                    Enhance AI{" "}
                 </h1>
                 <p className="text-lg text-gray-500">
-                    Upload your Image and let AI enhance to in seconds!
+                    AI-Powered Image Enhancement in Just Seconds.
                 </p>
             </div>
 
             <Home />
 
             <div className="text-sm text-gray-500 mt-6">
-                Powered By @SheryiansAI
+                Made by Kanishka
             </div>
         </div>
     );
