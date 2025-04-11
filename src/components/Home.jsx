880b37e96d12568b36d3d531b2d5a4040b81ff7d
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-11 19:46:27 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,36 @@
-import React, { useState } from 'react'
-import ImageUpload from "./ImageUpload"
-import ImagePreview from "./ImagePreview"
-import { enhancedImageAPI } from '../utils/enhanceImageAPI';
+import ImageUpload from "./ImageUpload";
+import ImagePreview from "./ImagePreview";
+import { useState } from "react";
+import { enhancedImageAPI } from "../utils/enhanceImageApi";
 
 const Home = () => {
-  const [uploadImage, setUploadImage] = useState(null);
-  const [enhancedImage, setEnhancedImage] = useState(null);
-  const [loading, setLoading] = useState(false);
+    const [uploadImage, setUploadImage] = useState(null);
+    const [enhancedImage, setEnhancedImage] = useState(null);
+    const [loading, setloading] = useState(false);
 
-  const UploadImageHandler = async(file) => {
-    setUploadImage(URL.createObjectURL(file));
-    setLoading(true);
+    const UploadImageHandler = async (file) => {
+        setUploadImage(URL.createObjectURL(file));
+        setloading(true);
+        try {
+            const enhancedURL = await enhancedImageAPI(file);
+            setEnhancedImage(enhancedURL);
+            setloading(false);
+        } catch (error) {
+            console.log(error);
+            alert("Error while enhancing the image. Please try again later.");
+        }
+    };
 
-    try {
-      const enhancedURL = await enhancedImageAPI(file);
-      setEnhancedImage(enhancedURL);
-      setLoading(false);      
-    } catch (error) {
-      console.log(error);
-      alert("Error while enhancing the image. Please try again later!");      
-    }
-  };
+    return (
+        <>
+            <ImageUpload UploadImageHandler={UploadImageHandler} />
+            <ImagePreview
+                loading={loading}
+                uploaded={uploadImage}
+                enhanced={enhancedImage?.image}
+            />
+        </>
+    );
+};
 
-
-  return (
-    <>
-      <ImageUpload UploadImageHandler={UploadImageHandler} />
-      <ImagePreview loading={loading} uploaded={uploadImage} enhanced={enhancedImage} />
-    </>
-  )
-}
-
-export default Home
+export default Home;
