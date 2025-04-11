767e0a9212ca7417f1e25eb44517f4c2c7425861
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-11 19:25:00 +0530

    Update enhanceImageAPI.js

--- src/utils/enhanceImageAPI.js
+++ src/utils/enhanceImageAPI.js
@@ -4,8 +4,11 @@ const BASE_URL = "https://techhk.aoscdn.com/";
 export const enhancedImageAPI = async(file) => {
     try {
         //code to upload image
+        // "/api/tasks/visual/scale"  --post
 
-        //fetch enhanced image        
+        //fetch enhanced image 
+        // "/api/tasks/visual/scale/{task_id}" --get
+       
     } catch (error) {
         console.log("Error enhancing image:", error.message);
         
